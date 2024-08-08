03507cf1a83f605721340ecf949442ae69ecae25
Ben <127999784+bnzrk@users.noreply.github.com>
2024-08-07 20:04:01 -0400

    Simplify paint info, fix scroll bar styling

--- src/paintinfo.cpp
+++ src/paintinfo.cpp
@@ -2,13 +2,7 @@
 
 PaintInfo::PaintInfo(const QImage &original, const QImage &modified, QRect paintRegion) :
     m_paintRegion{paintRegion},
-    m_original{QImage(paintRegion.size(), QImage::Format_ARGB32)},
-    m_modified{QImage(paintRegion.size(), QImage::Format_ARGB32)}
+    m_original{original.copy(paintRegion)},
+    m_modified{modified.copy(paintRegion)}
 {
-    // Initialize image regions
-    QPainter painter(&m_original);
-    painter.drawImage(m_original.rect(), original, m_paintRegion);
-    painter.end();
-    painter.begin(&m_modified);
-    painter.drawImage(m_modified.rect(), modified, m_paintRegion);
 }
